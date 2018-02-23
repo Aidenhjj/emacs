110f872df213cf555f2089e34c7b45d9d14f2aba
Aiden Jeffrey <aiden.jeffrey@codethink.co.uk>
2018-02-23 16:33:54 +0000

    Improve org-mode config

--- packages.el
+++ packages.el
@@ -53,6 +53,17 @@
 ;;    )
 ;;   )
 
+;; org-mode
+(use-package org
+  :ensure t
+  :mode ("\\.org\\'" . org-mode)
+  :bind (("C-c l" . org-store-link)
+         ("C-c a" . org-agenda))
+  :config
+  (progn
+    (setq org-log-done t))
+  )
+
 ;; counsel
 (use-package counsel
   :ensure
